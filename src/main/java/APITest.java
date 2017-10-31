fdb64f490eae9e31dc40a290404551772cbef27f
murphysb <murphysb@plu.edu>
2017-10-30 22:27:09 -0700

    working real time frequency detection program

--- src/main/java/APITest.java
+++ src/main/java/APITest.java
@@ -1,4 +1,8 @@
+import be.tarsos.dsp.*;
+import be.tarsos.dsp.io.jvm.AudioDispatcherFactory;
 import be.tarsos.dsp.pitch.*;
+
+import javax.sound.sampled.LineUnavailableException;
 
 /**
  * Created by Sean Murphy on 10/20/2017.
@@ -7,6 +11,20 @@ public class APITest {
 
     public static void main(String[] args){
         System.out.println("hello");
-        
+        PitchDetectionHandler handler = new PitchDetectionHandler() {
+            @Override
+            public void handlePitch(PitchDetectionResult pitchDetectionResult, AudioEvent audioEvent) {
+                System.out.println(audioEvent.getTimeStamp() + " " + pitchDetectionResult.getPitch());
+
+            }
+        };
+        try {
+            AudioDispatcher adp = AudioDispatcherFactory.fromDefaultMicrophone(2048, 0);
+            adp.addAudioProcessor(new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.YIN, 44100, 2048, handler));
+            adp.run();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ src/main/java/PitchComparison.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Sean Murphy on 10/30/2017.
+ */
+public class PitchComparison {
+
+    private float pitch;
+    private char[] notes;
+
+}
